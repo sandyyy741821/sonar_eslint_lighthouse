326629948adf20e5d4f7277fff8241a1e501ee88
sandyyy741821 <161047241+sandyyy741821@users.noreply.github.com>
2025-03-25 12:48:25 +0530

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,6 @@
 export default [
   {
     ignores: ["node_modules/"], 
-  },
-  {
-    files: ["**/*.js"], 
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-    },
     rules: {
       "semi": ["error", "always"],
       "quotes": ["error", "double"],
