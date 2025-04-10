dab2908a91658d98b603386931f76cf4a5c58a59
sandyyy741821 <santhoshsandyyy741821@gmail.com>
2025-04-10 20:06:40 +0530

    Adding files

---
+++ Eslint_lighthouse/file2.js
@@ -0,0 +1,7 @@
+//This file will have unused variables, console logs, and var usage.
+let  x=  100;
+console.log("X:",x);
+let unusedVar = "I am not used";
+console.log("This is unusedVar:", unusedVar);
+
+console.log("The value of x is: " + x);
---
+++ Eslint_lighthouse/file4.js
@@ -0,0 +1,10 @@
+//The file will have arrow function formatting issues and missing return values.
+const square =x => x*x;
+console.log(square(4)); 
+
+
+const printMessage = message => {
+  console.log( message );
+};
+
+printMessage( "ESLint is fixing me!" );
